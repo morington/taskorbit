97940c194614b2940c57824f927114c79aa84e2a
IvanKirpichnikov <mmssvvvv570@gmail.com>
2024-02-04 04:31:08 +0300

    Replcae 'EnumType' to 'EnumMeta'

--- taskorbit/models.py
+++ taskorbit/models.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, fields
-from enum import EnumType
+from enum import EnumMeta
 from typing import Optional, Union
 
 from taskorbit.enums import Commands
@@ -10,7 +10,7 @@ class BaseType:
     def __post_init__(self):
         for field in fields(self):
             attr_sel = getattr(self, field.name)
-            if isinstance(field.type, EnumType):
+            if isinstance(field.type, EnumMeta):
                 if field.type.validate_key(attr_sel):
                     setattr(self, field.name, field.type[attr_sel])
             elif not isinstance(attr_sel, field.type):
